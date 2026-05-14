02ff257f4393ce2a42eaccb1ebadc943da30881f
GSreevadhani <sreevadhanig.22msc@kongu.edu>
2026-05-14 14:43:24 +0530

    feat:second largest element in an array

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # entries
+
+#second largest element in an array
+output:
+
+\entries> java second-largest.java
+5
+1
+2
+3
+4
+5
+The second largest element is: 4
---
+++ second-largest.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest element.");
+        } else {
+            System.out.println("The second largest element is: " + secondLargest);
+        }
+    }
+}
